5b762fe3cf5078edd921217dcd15066656d5eee5
popsof <popsof@gmail.com>
2016-11-28 20:57:27 +0900

    Modify member model Modify dining model

--- django_app/dining/models.py
+++ django_app/dining/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from versatileimagefield.fields import VersatileImageField
 
 
 class Restaurant(models.Model):
@@ -27,11 +28,42 @@ class Menu(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     description = models.TextField()
+    img = VersatileImageField('Image', upload_to='menu_imgs')
 
 
+class Review(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    restaurant = models.ForeignKey(Restaurant)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    review_score = models.IntegerField()
+    review_like = models.IntegerField()
+    review_dislike = models.IntegerField()
+    created_date = models.DateTimeField(auto_now_add=True)
 
 
+class Restaurant_favor(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    restaurant = models.ForeignKey(Restaurant)
+    created_date = models.DateTimeField(auto_now_add=True)
 
 
+class Restaurant_img(models.Model):
+    restaurant = models.ForeignKey(Restaurant)
+    img = VersatileImageField('RestaurantImage', upload_to='restaurant_imgs')
+    alt = models.CharField(max_length=100)
+
+
+class Review_like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    review = models.ForeignKey(Review)
+    up_and_down = models.IntegerField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    
+
+class Review_img(models.Model):
+    review = models.ForeignKey(Review)
+    img = VersatileImageField('ReviewImage', upload_to='review_imgs')
+    alt = models.CharField(max_length=100)
 
 
--- django_app/member/models.py
+++ django_app/member/models.py
@@ -4,5 +4,10 @@ from versatileimagefield.fields import VersatileImageField
 
 
 class MyUser(AbstractUser):
-    profile_img = models.
+    nickname = models.CharField(max_length=30)
+    is_admin = models.BooleanField(default=False)
+    profile_img = VersatileImageField(
+                            'Profile',
+                            upload_to='profile_imgs')
+
 
