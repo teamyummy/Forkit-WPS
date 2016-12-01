59d72c5787864404f4b2213c4176ac0b605450c8
popsof <popsof@gmail.com>
2016-12-01 15:55:31 +0900

    Modify dining.serializers
    Add to_representation
    img_t for thumbnail image
    img_s for small image

--- django_app/dining/serializers.py
+++ django_app/dining/serializers.py
@@ -11,6 +11,19 @@ class MenuSerializer(serializers.ModelSerializer):
         fields = ('id', 'restaurant',
                   'name', 'price', 'description', 'img')
 
+    def to_representation(self, obj):
+        ret = super().to_representation(obj)
+
+        img_t = obj.img.thumbnail['200x10000'].url
+        img_s = obj.img.thumbnail['400x10000'].url
+
+        request = self.context.get('request', None)
+        if request is not None:
+            ret['img_t'] = request.build_absolute_uri(img_t)
+            ret['img_s'] = request.build_absolute_uri(img_s)
+
+        return ret
+
 
 class RestaurantImgSerializer(serializers.ModelSerializer):
     class Meta:
